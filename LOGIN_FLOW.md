2db1a4e54e90301cb9fbf97d5c95e0af3cdeb7c9
Daniel Wandarti <dfwandarti@hotmail.com>
2026-08-23 10:43:26 -0300

    broke html into files

---
+++ LOGIN_FLOW.md
@@ -0,0 +1,6 @@
+```mermaid
+flowchart TD
+    LOGIN["Login"] --Entrar--> HOME["Home"]
+    HOME["Home"] --Minhas compras--> COMPRAS["Minhas compras"]
+    COMPRAS --Pesquisar--> COMPRAS_FILTRADAS["Compras de um dia"]
+```
--- python/page_field/display_name_to_test_id.py
+++ python/page_field/display_name_to_test_id.py
@@ -4,6 +4,11 @@ display_name_to_test_id = {
   "login - botão logar": "botao-entrar",
   "login - mensagem erro": "mensagem-erro",
   "home - mensagem sucesso": "mensagem-sucesso",
+  "home - botão minha conta": "botao-minha-conta",
+  "home - botão minhas compras": "botao-minhas-compras",
+  "minhas compras - input data pesquisa": "input-data-pesquisa",
+  "minhas compras - botão pesquisar": "botao-pesquisar",
+  "minhas compras - tabela compras": "tabela-compras",
   "login - logo wesayso": "wesayso-icon",
   "login - logo acme": "acme-icon",
 }
